df303c311f1918e74010f594ab9a5f49d2554696
Rafael Bertuzzi <rafaelbertuzzi@MacBook-Pro-de-Rafael.local>
2023-09-17 23:54:05 -0300

    chore: modify husky pre-commit and add path alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@features': './src/features',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
