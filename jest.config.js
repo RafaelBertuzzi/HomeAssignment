d4756067c4fa7ae6fad0555ac7b2fdd715ee21c0
Rafael Bertuzzi <rafaelbertuzzi@MacBook-Pro-de-Rafael.local>
2023-09-18 01:00:33 -0300

    chore: add ci lint step and add coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  coverageThreshold: {
+    global: {
+      lines: 95,
+    },
+  },
+  testMatch: ['**/*.test.ts?(x)'],
 };
