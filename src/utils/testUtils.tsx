dd242cc4260ec6354976abbaabe00c9b6418b7e5
Rafael Bertuzzi <rafaelbertuzzi@MacBook-Pro-de-Rafael.local>
2023-09-19 12:06:24 -0300

    test: fix test utils with redux provider

--- src/utils/testUtils.tsx
+++ src/utils/testUtils.tsx
@@ -1,13 +1,25 @@
 import { ReactNode } from 'react';
+import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 
 import { LayoutProvider } from '@components/LayoutProvider';
+import { store } from '@redux/store';
 import { render } from '@testing-library/react-native';
 
 export const rendererWithProviders = (children: ReactNode) => {
-  return renderer.create(<LayoutProvider>{children}</LayoutProvider>).toJSON();
+  return renderer
+    .create(
+      <Provider store={store}>
+        <LayoutProvider>{children}</LayoutProvider>
+      </Provider>,
+    )
+    .toJSON();
 };
 
 export const renderWithProviders = (children: ReactNode) => {
-  return render(<LayoutProvider>{children}</LayoutProvider>).toJSON();
+  return render(
+    <Provider store={store}>
+      <LayoutProvider>{children}</LayoutProvider>
+    </Provider>,
+  ).toJSON();
 };
